3bc6e3953894bb952f217ba4492d8af613dc2e45
Martin Macura <22004964+2bad2furious@users.noreply.github.com>
2024-04-19 20:14:51 +0200

    Create page.jsx

---
+++ abc/def/page.jsx
@@ -0,0 +1,10 @@
+/**
+ * Složky ve složce src/app diktují adresu, na které bude stránka k dispozici
+ *  Tahle bude na localhost:3000/abc/def
+ *  
+ *  Stránky mohou mít také koncovku .jsx
+ * @returns {JSX.Element}
+ */
+export default function SomePage(){
+    return <>some content on abc/def</>
+}
